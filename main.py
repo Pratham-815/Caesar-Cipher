cf8838ba0e3457154bf476e7cc053af39c750f92
Pratham Agarwal <143127428+Pratham-815@users.noreply.github.com>
2024-02-14 07:55:06 +0000

    Function to decrypt message

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ direction = input("Type encode to 'encrypt' and decode to 'decrypt':\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# Creating a funtion to encrypt the code
+# Creating a funtion to encrypt the message
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for char in plain_text:
@@ -17,3 +17,17 @@ def encrypt(plain_text, shift_amount):
         else:
             cipher_text += char
     print(f"The encoded text message is: {cipher_text}")
+
+# Creating a function to decrypt the message
+def decrypt(encoded_text, shift_amount):
+    shift_amount *= -1
+    plain_text = ""
+    for char in encoded_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            plain_text += alphabet[new_position]
+        else:
+            plain_text += char
+    print(f"The decoded text message is: {plain_text}")
+    
