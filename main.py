079c4ab567ee486ec6637a417457c7d46649e267
Pratham Agarwal <143127428+Pratham-815@users.noreply.github.com>
2024-02-14 03:16:52 +0000

    Create a function to encrypt message

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type encode to 'encrypt' and decode to 'decrypt':\n").lower()
-text = input("Type your message:\n")
+text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+# Creating a funtion to encrypt the code
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for char in plain_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            cipher_text += alphabet[new_position]
+        else:
+            cipher_text += char
+    print(f"The encoded text message is: {cipher_text}")
